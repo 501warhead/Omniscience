d9de9307bc828d15f2eb8224ae09471a2fb3ba5d
CommissarVoop <viperlv2@inbox.lv>
2022-02-13 14:57:23 +0200

    Updated utils

--- OmniscienceAPI/src/main/java/io/github/warhead501/omniscience/api/util/OmniUtils.java
+++ OmniscienceAPI/src/main/java/io/github/warhead501/omniscience/api/util/OmniUtils.java
@@ -1,9 +1,12 @@
 package io.github.warhead501.omniscience.api.util;
 
+import io.github.warhead501.omniscience.api.OmniApi;
 import lombok.experimental.UtilityClass;
-import lv.voop.essn.paper.utils.item.ItemUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
 
 @UtilityClass
 public class OmniUtils {
@@ -13,7 +16,15 @@ public class OmniUtils {
         return true;
     }
     public static boolean isOmniTool(ItemStack item) {
-        if (ItemUtils.hasCustomTag(item,"omnisciencetool")) return true;
-        return false;
+        var meta = item.getItemMeta();
+        if (meta==null) return false;
+        var container = meta.getPersistentDataContainer();
+        return container.has(OmniApi.getOmniscience().getItemKey(),PersistentDataType.STRING);
+    }
+
+    @NotNull
+    public static String getNMSVersion() {
+        String name = Bukkit.getServer().getClass().getPackage().getName();
+        return name.substring(name.lastIndexOf('.') + 1);
     }
 }
